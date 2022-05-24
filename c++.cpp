c36bafe923c0f706b78c61997ca75b22a24548d6
Nupur1779 <101565915+Nupur1779@users.noreply.github.com>
2022-05-24 11:19:48 -0700

    Update c++.cpp

---
+++ c++.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class shape{
+    public:
+    shape(){
+        cout<<"default constructor in shape"<<endl;
+    }
+    
+};
+class twoshape:public shape{
+    public:
+    twoshape(){
+        cout<<"default constructor in twoshape"<<endl;
+    }
+    twoshape(int y){
+        cout<<"this is 2d shape"<<endl;
+    }
+};
+class threeshape:public twoshape{
+    public:
+    threeshape(int z){
+        cout<<"this is 3d shape"<<endl;
+    }
+};
+int main()
+{
+    shape p1;
+    cout<<"hello \n";
+    twoshape p2(4);
+    cout<<"hello \n ";
+    threeshape p3(2);
+    return 0;
+}
